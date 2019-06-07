b09725e145b140d84920a31f30fae658088f3f95
vu duy <49891493+ahihiduy26@users.noreply.github.com>
2019-06-07 16:40:33 +0700

    Update script.js

--- BaiTapCuoiKi/jquery/script.js
+++ BaiTapCuoiKi/jquery/script.js
@@ -1,26 +1,10 @@
 $(document).ready(function(){
-    $( "#dialog" ).dialog({
-      autoOpen: false,
-      show: {
-        effect: "blind",
-        duration: 1000
-      },
-      hide: {
-        effect: "explode",
-        duration: 1000
-      },
-			modal : true,
-			button : {
-				"Log in" : function(){
-					alert('Test Log in');
-				},
-				"Exit" : function(){
-					dialog.dialog('close')
-				}
-			}
-    });
-
-    $( "#opener" ).on( "click", function() {
-      $( "#dialog" ).dialog( "open" );
-    });
-  } );
+  var a = $('input')
+  $("#bntt").click(function(){
+    if( a == 0){
+      alert("You have not entered")
+    }else{
+      alert("Your post has been sent")
+    }
+  })
+})
